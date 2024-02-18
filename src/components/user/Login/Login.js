7c50f636a91d8c76ff5c46272b0c3cb671ec0adb
Ренета Бонева <reneta.boneva@abv.bg>
2024-02-18 17:56:59 +0200

    Control forms state

--- src/components/user/Login/Login.js
+++ src/components/user/Login/Login.js
@@ -1,6 +1,11 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 
 export function Login() {
+    const [values, setValues] = useState({
+        email: "",
+        password: ""
+    })
+
     useEffect(() => {
         document.querySelector('div.contentWrapper').style.height = '70vh'
         return () => {
@@ -8,15 +13,35 @@ export function Login() {
         }
     }, [])
 
+    function handleChange(e) {
+        setValues(values => ({ ...values, [e.target.name]: e.target.value }));
+    }
+
+    function handleLoginSubmit(e) {
+        e.preventDefault();
+
+        console.log(values);
+    }
+
     return (
-        <form method="POST" id="loginForm">
+        <form method="POST" id="loginForm" onSubmit={handleLoginSubmit}>
             <div>
                 <label htmlFor="email" >Email</label>
-                <input name='email' type="email" />
+                <input
+                    name='email'
+                    type="email"
+                    value={values.email}
+                    onChange={handleChange}
+                />
             </div>
             <div>
                 <label htmlFor="password" >Password</label>
-                <input name="password" type="password" />
+                <input
+                    name="password"
+                    type="password"
+                    value={values.password}
+                    onChange={handleChange}
+                />
             </div>
             <input type="submit" value='LOGIN' />
         </form>
--- src/components/user/Register/Register.js
+++ src/components/user/Register/Register.js
@@ -1,6 +1,13 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 
 export function Register() {
+    const [values, setValues] = useState({
+        email: "",
+        username: "",
+        password: "",
+        rePass: "",
+    })
+
     useEffect(() => {
         document.querySelector('div.contentWrapper').style.height = '70vh'
         return () => {
@@ -8,23 +15,57 @@ export function Register() {
         }
     }, [])
 
+    function handleChange(e) {
+        setValues(values => ({ ...values, [e.target.name]: e.target.value }));
+    }
+
+    function handleRegisterSubmit(e) {
+        e.preventDefault();
+
+        console.log(values);
+    }
+
     return (
-        <form method="POST" id="registerForm">
-            <div>
+        <form method="POST" id="registerForm" onSubmit={handleRegisterSubmit}>
+            <div> 
                 <label htmlFor="email" >Email</label>
-                <input name='email' type="email" />
+                <input
+                    name='email'
+                    type="email"
+                    value={values.email}
+                    onChange={handleChange}
+
+                />
             </div>
             <div>
                 <label htmlFor="username" >Username</label>
-                <input name='username' type="text" />
+                <input
+                    name='username'
+                    type="text"
+                    value={values.username}
+                    onChange={handleChange}
+
+                />
             </div>
             <div>
                 <label htmlFor="password" >Password</label>
-                <input name="password" type="password" />
+                <input
+                    name="password"
+                    type="password"
+                    value={values.password}
+                    onChange={handleChange}
+
+                />
             </div>
             <div>
                 <label htmlFor="rePass" >Repeat password</label>
-                <input name="rePass" type="password" />
+                <input
+                    name="rePass"
+                    type="password"
+                    value={values.rePass}
+                    onChange={handleChange}
+
+                />
             </div>
             <input type="submit" value='Register' />
         </form>
