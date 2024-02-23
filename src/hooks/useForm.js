402ed032c1630e669aa5aa2f2af5129ed40ac439
Ренета Бонева <reneta.boneva@abv.bg>
2024-02-23 19:29:59 +0200

    Modify useForm

---
+++ src/hooks/useForm.js
@@ -0,0 +1,21 @@
+const { useState } = require("react");
+
+export function useForm(initValues, submitHandler) {
+    const [values, setValues] = useState(initValues);
+
+    function changeValues(e) {
+        setValues(state => ({ ...state, [e.target.name]: e.target.value }));
+    }
+
+    function onSubmit(e){
+        e.preventDefault();
+        submitHandler(values);
+        setValues(initValues);
+    }
+
+    return [
+        values,
+        changeValues,
+        onSubmit
+    ];
+}
