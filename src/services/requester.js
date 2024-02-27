0493430c6c51d50c5ed48aa80d6653d0eb0bf9c3
Ренета Бонева <reneta.boneva@abv.bg>
2024-02-27 23:06:59 +0200

    Add  basic requester

---
+++ src/services/requester.js
@@ -0,0 +1,31 @@
+
+const serverAPI = 'https://server-game-play-hub.onrender.com';
+
+async function req(method, url, data) {
+    const options = {};
+
+    if (method !== 'GET') {
+        options.method = method;
+        if (data) {
+            options.headers = {
+                'Content-Type': 'application/json',
+            }
+            options.body = JSON.stringify(data);
+        }
+    }
+
+    const res = await fetch(`${serverAPI}${url}`, options);
+
+    const result = await res.json();
+
+    console.log(result);
+
+    return result;
+}
+
+export const requester = {
+    get: req.bind(null, 'GET'),
+    post: req.bind(null, 'POST'),
+    put: req.bind(null, 'PUT'),
+    delete: req.bind(null, 'DELETE'),
+}
