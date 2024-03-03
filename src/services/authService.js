3794ee1dacd565c87499f50610c92f242c5c83c3
Ренета Бонева <reneta.boneva@abv.bg>
2024-03-03 22:02:09 +0200

    Add logout in authService

--- src/services/authService.js
+++ src/services/authService.js
@@ -3,3 +3,5 @@ import { requester } from "./requester";
 export const login = async (loginData) => requester.post('/login', loginData);
 
 export const register = async (registerData) => requester.post('/register', registerData);
+
+export const logout = async () => requester.get('/logout');
