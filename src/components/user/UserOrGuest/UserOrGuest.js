850704998f083b15226f75af68321003e9457e59
Ренета Бонева <reneta.boneva@abv.bg>
2024-03-11 15:15:27 +0200

    Add register form to UserOrGuest component

--- src/components/user/UserOrGuest/UserOrGuest.js
+++ src/components/user/UserOrGuest/UserOrGuest.js
@@ -1,12 +1,16 @@
 import styles from "./UserOrGuest.module.css"
 import { Login } from "../Login/Login";
 import { GuestRegistrationForm } from "./GuestRegistrationForm/GuestRegisterForm.js";
+import { Register } from "../Register/Register.js";
 
 export function UserOrGuest() {
     return (
         <div className='contentWrapper mainWrapper'>
             <h2 className={styles.h2}>Continue as gamer</h2>
             <Login />
+            <hr className={styles.hr}></hr>
+            <h2 className={styles.h2}>You don't have registration?</h2>
+            <Register />
             <hr className={styles.hr}></hr>
             <h2 className={styles.h2}>Continue as guest</h2>
             <GuestRegistrationForm />
