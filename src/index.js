a724d938bfcdba46a93ca5cb00887fc7b47e04bf
Ренета Бонева <reneta.boneva@abv.bg>
2024-03-17 15:26:33 +0200

    Remove redux provider

--- src/index.js
+++ src/index.js
@@ -6,16 +6,13 @@ import { Provider } from 'react-redux'
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { store } from './redux/store'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
-    </Provider>
   </React.StrictMode>
 );
 
