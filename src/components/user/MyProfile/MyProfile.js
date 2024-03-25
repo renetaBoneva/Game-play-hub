ed0bd1eacb924ed37a6847101596566429898c82
Ренета Бонева <reneta.boneva@abv.bg>
2024-03-25 23:31:47 +0200

    Working on My profile page

--- src/components/user/MyProfile/MyProfile.js
+++ src/components/user/MyProfile/MyProfile.js
@@ -1,39 +1,49 @@
 import { useEffect, useState } from "react";
 import { useAuthContext } from "../../../hooks/useAuthContext";
+import { toast } from "react-toastify";
 
 import { getUserById } from "../../../services/authService";
 import './MyProfile.css'
-import { toast } from "react-toastify";
+import { Loading } from "../../common/Loading/Loading";
 
 export function MyProfile() {
     const [userInfo, setUserInfo] = useState({});
+    const [isLoading, setIsLoading] = useState(true);
     const { _userID } = useAuthContext();
 
     useEffect(() => {
         getUserById(_userID)
-            .then(res => setUserInfo(res))
-            .catch(err => toast.error(err))
-    }, [_userID]);
+            .then(res => {
+                setUserInfo(res)
+                setIsLoading(false);
+            })
+            .catch(err => {
+                toast.error(err);
+                setIsLoading(false);
+            });
+    });
 
     return (
         <>
-            <div className="myProfileWrapper">
-                {userInfo && (<>
-                    <h2>My profile</h2>
-                    <div className="myProfileInfoWrapper">
-                        {userInfo && userInfo.email && (
-                            <div className="infoWrapper">
-                                <p className="infoLabel">Email:</p>
-                                <p className="infoP">{userInfo.email}</p>
-                            </div>)}
+            {isLoading
+                ? <Loading />
+                : <div className="myProfileWrapper">
+                    {Object.keys(userInfo).length > 0 && (<>
+                        <h2>My profile</h2>
+                        <div className="myProfileInfoWrapper">
+                            {Object.keys(userInfo).length > 0 && userInfo.email && (
+                                <div className="infoWrapper">
+                                    <p className="infoLabel">Email:</p>
+                                    <p className="infoP">{userInfo.email}</p>
+                                </div>)}
 
-                        {userInfo && userInfo.username && (
-                            <div className="infoWrapper">
-                                <p className="infoLabel">Username:</p>
-                                <p className="infoP">{userInfo.username}</p>
-                            </div>)}
-                    </div> </>)}
-            </div>
+                            {Object.keys(userInfo).length > 0 && userInfo.username && (
+                                <div className="infoWrapper">
+                                    <p className="infoLabel">Username:</p>
+                                    <p className="infoP">{userInfo.username}</p>
+                                </div>)}
+                        </div> </>)}
+                </div>}
         </>
     );
 }
