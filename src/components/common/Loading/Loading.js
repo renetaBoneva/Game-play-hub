e3adb99ae6ce46d4d4dd0aaa0b016a1fb1880206
Ренета Бонева <reneta.boneva@abv.bg>
2024-03-26 11:49:21 +0200

    Working on Loading components css

--- src/components/common/Loading/Loading.js
+++ src/components/common/Loading/Loading.js
@@ -1,5 +1,8 @@
 import styles from './Loading.module.css'
 
 export function Loading() {
-    return <div className={styles.loader}></div> 
+    return (
+    <div className='contentWrapper'>
+        <div className={styles.loader}></div>
+    </div>)
 }
