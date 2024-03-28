aca2d3670607e079e13f715f4a1112c3124117aa
Ренета Бонева <reneta.boneva@abv.bg>
2024-03-28 09:58:48 +0200

    Add loading in Catalog section

--- src/components/Catalog/Catalog.js
+++ src/components/Catalog/Catalog.js
@@ -4,29 +4,37 @@ import { useEffect, useState } from "react";
 import './Catalog.css'
 import * as gameService from '../../services/gamesService';
 import { toast } from "react-toastify";
+import { Loading } from "../common/Loading/Loading";
 
 export function Catalog() {
     const [games, setGames] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
         gameService.getAllGames()
             .then(res => {
                 setGames(res);
+                setIsLoading(false);
             })
             .catch(err => toast.error(err.message));
 
     }, []);
 
-    return (
-        <div className="gameCoverWrapper">
-            {games
-                ? games.map(game => {
-                    return (
-                        <div className="gameCover" key={game._id}>
-                            <Link to={game.link}><img alt={game.name} src={game.coverImgPath} /></Link>
-                        </div>
-                    )})
-                : <h2>There are no available games.</h2>
-            }
-        </div>);
+    if (isLoading) {
+        return <Loading />
+    } else {
+        return (
+            <div className="gameCoverWrapper">
+                {games
+                    ? games.map(game => {
+                        return (
+                            <div className="gameCover" key={game._id}>
+                                <Link to={game.link}><img alt={game.name} src={game.coverImgPath} /></Link>
+                            </div>
+                        )
+                    })
+                    : <h2>There are no available games.</h2>
+                }
+            </div>);
+    }
 }
